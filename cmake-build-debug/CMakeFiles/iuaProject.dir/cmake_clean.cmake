99cf913d1ab9f06210604c5d9a40a4869aa240db
Agustin <agustin@draimo.com>
2020-08-15 12:10:08 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/iuaProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iuaProject.dir/main.cpp.obj"
+  "iuaProject.exe"
+  "iuaProject.exe.manifest"
+  "iuaProject.pdb"
+  "libiuaProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iuaProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
