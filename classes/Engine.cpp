643e9aa0c194cdafed216e4df51696f34e12225f
Agustin <agustin@draimo.com>
2020-08-28 08:43:44 -0300

    [Engine] Creacion de funciones basicas

--- classes/Engine.cpp
+++ classes/Engine.cpp
@@ -3,7 +3,37 @@
 //
 
 #include "Engine.h"
+using namespace sf;
 
-class Engine {
+Engine::Engine() {
+    // Setea la resolucion con la que se va a jugar
+    Vector2f resolution;
+    resolution.x = 1350;
+    resolution.y = 600;
 
-};
+    window.create(VideoMode(resolution.x, resolution.y),
+                  "IUA Game project",
+                  Style::Default);
+
+    // Carga la textura del fondo en una textura
+    bkgTexture.loadFromFile("../assets/background1.jpg");
+
+    // Setea el sprite en base a la textura
+    bkgSprite.setTexture(bkgTexture);
+
+}
+
+void Engine::start() {
+    // Timing
+    Clock clock;
+
+    while(window.isOpen()) {
+        Time dt = clock.restart();
+
+        float dtAsSeconds = dt.asSeconds();
+
+        input();
+        update(dtAsSeconds);
+        draw();
+    }
+}
--- classes/Engine.h
+++ classes/Engine.h
@@ -2,13 +2,30 @@
 // Created by Agus on 28/8/2020.
 //
 
-#ifndef IUAPROJECT_ENGINE_H
-#define IUAPROJECT_ENGINE_H
+#include <SFML/Graphics.hpp>
+#include "Jake.h"
 
+using namespace sf;
 
 class Engine {
+private:
+    RenderWindow window;
+
+    Sprite bkgSprite;
+    Texture bkgTexture;
+
+    // Instancia de Jake
+    Jake jake;
+
+    // Funciones para el funcionamiento
+    void input();
+    void update(float dtAsSeconds);
+    void draw();
+
+public:
+    Engine();
+
+    // Inicia el juego llamando a todas las funciones privadas
+    void start();
 
 };
-
-
-#endif //IUAPROJECT_ENGINE_H
