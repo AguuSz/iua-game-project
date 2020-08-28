60e60746bd39d0eda69d6a7ae8fdd6dcf6f7cb73
Agustin <agustin@draimo.com>
2020-08-28 08:44:58 -0300

    [Update] Creacion de funciones basicas

--- classes/Update.cpp
+++ classes/Update.cpp
@@ -1,4 +1,11 @@
 //
 // Created by Agus on 28/8/2020.
 //
+#include "Engine.h"
+using namespace sf;
 
+void Engine::update(float dtAsSeconds) {
+    jake.update(dtAsSeconds);
+
+    // Falta la deteccion de colision...
+}
