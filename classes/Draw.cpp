d67d389182d42f040678a066246d84b0e7c52d9a
Agustin <agustin@draimo.com>
2020-08-28 08:43:08 -0300

    [Draw] Creacion de metodos esenciales para dibujo

--- classes/Draw.cpp
+++ classes/Draw.cpp
@@ -1,4 +1,17 @@
 //
 // Created by Agus on 28/8/2020.
 //
+#include "Engine.h"
+
+void Engine::draw() {
+    // Limpia la pantalla
+    window.clear();
+
+    // Dibuja el fondo
+    window.draw(bkgSprite);
+    window.draw(jake.getSprite());
+
+    // Mostrar lo que hemos dibujado
+    window.display();
+}
 
